081ceec41a28607820a2772f523fa98ac86b8923
Gabor35 <pusztaig@kkszki.hu>
2025-04-01 11:03:42 +0200

    Add project files.

---
+++ MauiApp3/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+using System.Net.Http;
+using MauiApp3.Services;
+using MauiApp3;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureMauiHandlers(handlers =>
+            {
+                handlers.AddHandler<BlazorWebView, BlazorWebViewHandler>();
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        builder.Services.AddSingleton<HttpClient>(sp =>
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://esemenyrendezo1.azurewebsites.net/api/")
+            };
+            return httpClient;
+        });
+
+        builder.Services.AddSingleton<ApiService>();
+
+        return builder.Build();
+    }
+}
---
+++ MauiApp3/Models/SavedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Models
+{
+    public class SavedEvent
+    {
+        public int Id { get; set; }
+        public string Cime { get; set; }
+        public string Helyszin { get; set; }
+        public string KepUrl { get; set; }
+        public DateTime Datum { get; set; }
+        public string Leiras { get; set; }
+    }
+}
